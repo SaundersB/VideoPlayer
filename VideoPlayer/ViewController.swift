babd8f81312430de72701a25739caf323852da5a
SaundersB <speakeasys@gmail.com>
2016-06-05 10:22:47 -0700

    Adding button controls that appear or disappear depending on if you're playing a video or not.

--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -16,6 +16,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var play_button: UIButton!
     var player = AVPlayer()
+    var playerLayer = AVPlayerLayer()
+    let pause_button = UIButton(frame: CGRect(x: 100, y: 500, width: 75, height: 35))
+    let resume_button = UIButton(frame: CGRect(x: 200, y: 500, width: 75, height: 35))
+    let destroy_video_player_button = UIButton(frame: CGRect(x: 300, y: 500, width: 75, height: 35))
 
     
     override func viewDidLoad() {
@@ -28,29 +32,34 @@ class ViewController: UIViewController {
     @IBAction func play_online_video(sender: AnyObject) {
         let videoURL = NSURL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
         self.player = AVPlayer(URL: videoURL!)
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = self.view.bounds
+        self.playerLayer = AVPlayerLayer(player: player)
+        self.playerLayer.frame = self.view.bounds
         self.view.layer.addSublayer(playerLayer)
-        player.closedCaptionDisplayEnabled = true
-        player.play()
+        self.player.play()
         create_video_controls()
     }
     
     func create_video_controls() {
-        let button = UIButton(frame: CGRect(x: 100, y: 500, width: 75, height: 35))
-        button.backgroundColor = .blueColor()
-        button.layer.cornerRadius = 5
-        button.setTitle("Pause", forState: .Normal)
-        button.addTarget(self, action: #selector(pauseButtonAction), forControlEvents: .TouchUpInside)
-        self.view.addSubview(button)
+        pause_button.hidden = false
+        pause_button.backgroundColor = .blueColor()
+        pause_button.layer.cornerRadius = 5
+        pause_button.setTitle("Pause", forState: .Normal)
+        pause_button.addTarget(self, action: #selector(pauseButtonAction), forControlEvents: .TouchUpInside)
+        self.view.addSubview(pause_button)
         
-        let resume_button = UIButton(frame: CGRect(x: 200, y: 500, width: 75, height: 35))
+        resume_button.hidden = false
         resume_button.backgroundColor = .blueColor()
         resume_button.layer.cornerRadius = 5
         resume_button.setTitle("Play", forState: .Normal)
         resume_button.addTarget(self, action: #selector(resumeButtonAction), forControlEvents: .TouchUpInside)
         self.view.addSubview(resume_button)
 
+        destroy_video_player_button.hidden = false
+        destroy_video_player_button.backgroundColor = .blueColor()
+        destroy_video_player_button.layer.cornerRadius = 5
+        destroy_video_player_button.setTitle("Close", forState: .Normal)
+        destroy_video_player_button.addTarget(self, action: #selector(destroyVideoPlayer), forControlEvents: .TouchUpInside)
+        self.view.addSubview(destroy_video_player_button)
     }
     
     func pauseButtonAction(sender: UIButton!) {
@@ -61,6 +70,17 @@ class ViewController: UIViewController {
     func resumeButtonAction(sender: UIButton!) {
         print("Video resumed")
         self.player.play()
+    }
+    
+    func destroyVideoPlayer(sender: UIButton!) {
+        dispatch_async(dispatch_get_main_queue()) {
+            self.player.pause()
+            self.playerLayer.removeFromSuperlayer()
+        }
+        pause_button.hidden = true
+        resume_button.hidden = true
+        destroy_video_player_button.hidden = true
+        
     }
     
 }
